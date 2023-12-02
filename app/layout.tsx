769e822b9c7532176596c87511703afe1f381f13
Muhsin-42 <muhsinofficial21@gmail.com>
2023-12-02 23:39:24 +0530

    build errors resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
-import { Inter, Space_Grotesk } from "next/font/google";
+import {
+  Inter,
+  // Space_Grotesk,
+  //  Space_Grotesk
+} from "next/font/google";
 import { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
@@ -18,11 +22,11 @@ const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-spaceGrotesk",
-});
+// const spaceGrotesk = Space_Grotesk({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "500", "600", "700"],
+//   variable: "--font-spaceGrotesk",
+// });
 
 export default function RootLayout({
   children,
@@ -31,7 +35,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      {/* <body className={`${inter.variable} ${spaceGrotesk.variable}`}> */}
+      <body className={`${inter.variable}`}>
         <ClerkProvider
           appearance={{
             elements: {
