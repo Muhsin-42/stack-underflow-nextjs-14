c3cb2116ea7675ff6747c5ab3e6508f9083fd8a1
Muhsin-42 <muhsinofficial21@gmail.com>
2023-12-04 07:54:15 +0530

    Mobile Navbar added

---
+++ components/shared/NavBar/MobileNav.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+const NavContent = () => {
+  return <div>nav content</div>;
+};
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side={"left"}
+        className="background-light900_dark200 border-none"
+      >
+        <Link href={"/"} className="flex items-center ">
+          <Image
+            src={"/assets/images/site-logo.svg"}
+            width={23}
+            height={23}
+            alt="DevFlow"
+          />
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Stack <span className="text-primary-500">Underflow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose>
+            <NavContent />
+          </SheetClose>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href={"/sign-in"}>
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Login</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
